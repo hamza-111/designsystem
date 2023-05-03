903ce1e8bcb08677ce93a33e4964112cb1d01efe
habbasi <hamza.abbasi@external.engie.com>
2023-05-03 16:39:55 +0200

    branch update

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,18 @@ var P = require('gulp-load-plugins')({
   config: S.pkg
 });
 
+gulp.task('deploy:dev', function() {
+  return gulp.src('dist/**/*')
+    .pipe(P.ghPages({
+      remoteUrl: "https://github.com/hamza-111/designsystem.git",
+      branch: "dev"
+    }));
+});
+
 gulp.task('deploy:main', function() {
   return gulp.src('dist/**/*')
     .pipe(P.ghPages({
       remoteUrl: "https://github.com/hamza-111/designsystem.git",
       branch: "main"
-    }));
-});
-
-gulp.task('deploy:master', function() {
-  return gulp.src('dist/**/*')
-    .pipe(P.ghPages({
-      remoteUrl: "https://github.com/hamza-111/designsystem.git",
-      branch: "master"
     }));
 });
